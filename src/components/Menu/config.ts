848fe7718a04c23674d079f1f36b81b1b74f33a6
huquduy <huquduy@gmail.com>
2021-06-18 20:31:35 +0700

    #5126

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -25,11 +25,11 @@ const config: MenuEntry[] = [
     items: [
       {
         label: 'Exchange',
-        href: isMainnet ? 'https://exchange.cowswap.app/#/swap' : 'https://exchange-testnet.cowswap.app/#/swap',
+        href: isMainnet ? '/' : 'https://exchange-testnet.cowswap.app/#/swap', // https://exchange.cowswap.app/#/swap
       },
       {
         label: 'Liquidity',
-        href: isMainnet ? 'https://exchange.cowswap.app/#/pool' : 'https://exchange-testnet.cowswap.app/#/pool',
+        href: isMainnet ? '/' : 'https://exchange-testnet.cowswap.app/#/pool', // https://exchange.cowswap.app/#/pool
       },
     ],
   },
