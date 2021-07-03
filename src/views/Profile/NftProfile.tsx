f20f1d60f2959faf6e8f8790c57024ce413e1ca1
huquduy <huquduy@gmail.com>
2021-07-04 00:49:56 +0700

    #5091

--- src/views/Profile/NftProfile.tsx
+++ src/views/Profile/NftProfile.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useCallback, useEffect, useMemo } from 'react'
 import BigNumber from 'bignumber.js';
 import { useWeb3React } from '@web3-react/core'
-import { Card, CardBody, Heading, Text, CardRibbon, LinkExternal } from '@cowswap/uikit'
+import { Card, CardBody, Heading, Text, CardRibbon, LinkExternal, Skeleton } from '@cowswap/uikit'
 import { NFT_URI, BASE_BSC_SCAN_URL } from 'config'
 import { getLuckyDrawNFTContract } from 'utils/contractHelpers'
 import multicall from 'utils/multicall'
@@ -17,7 +17,7 @@ const FlexLayout = styled.div`
   flex-wrap: wrap;
   & > * {
     min-width: 180px;
-    max-width: 29.8%;
+    max-width: 21.8%;
     width: 100%;
     margin: 0 8px;
     margin-bottom: 32px;
@@ -47,6 +47,7 @@ const FlexCardDetails = styled.div`
 const MAGIC_TYPE = 1
 
 const NftProfile = () => {
+  const [loading, setLoading] = useState(false)
   const [nfts, setNfts] = useState({})
   const [metas, setMetas] = useState([])
   const { currentBlock } = useBlock()
@@ -63,6 +64,7 @@ const NftProfile = () => {
     if (!account) {
       return []
     }
+    setLoading(true)
     const nftBalance = await luckyDrawNFTContract.methods.balanceOf(account).call()
     const tokenIdCalls= Array.from(Array(Number(nftBalance)).keys()).map(index => ({
       address: luckyDrawNFTAddress,
@@ -77,6 +79,7 @@ const NftProfile = () => {
     }))
 
     if (!tokenIds) {
+      setLoading(false)
       return {}
     }
 
@@ -93,11 +96,12 @@ const NftProfile = () => {
         method: 'metadatas',
         metadataCalls,
       })
+      setLoading(false)
       return []
     })
 
     setMetas(metadatas)
-
+    setLoading(false)
     return metadatas
   }, [account, luckyDrawNFTContract, luckyDrawNFTAddress])
 
@@ -130,23 +134,26 @@ const NftProfile = () => {
   return (
     <>
       <Heading size="lg" mb="15px">My gallery ({metas.length} nfts)</Heading>
-      <FlexLayout>
-        {Object.keys(nfts).map((type) => {
-          const { image, name, author, description, count } = nfts[type]
-          return (
-            <CardStyled key={image} p="0px" ribbon={<CardRibbon variantColor="success" text={name} />}>
-              <CardBody p="0px">
-                <img width="100%" src={image} alt="cow-nft" />
-              </CardBody>
-              <FlexCardDetails className="details">
-                <LinkExternal href={`${BASE_BSC_SCAN_URL}/address/${author}`}>Author address</LinkExternal>
-                <Text mt="15px">Amount: {count}</Text>
-                <Text mt="15px" color="textSubtle">Description: {description}</Text>
-              </FlexCardDetails>
-            </CardStyled>
-          )
-        })}
-      </FlexLayout>
+      { loading && !metas.length ?
+        <Skeleton /> :
+        <FlexLayout>
+          {Object.keys(nfts).map((type) => {
+            const { image, name, author, description, count } = nfts[type]
+            return (
+              <CardStyled key={type} p="0px" ribbon={<CardRibbon variantColor="success" text={name} />}>
+                <CardBody p="0px">
+                  <img width="100%" src={image} alt="cow-nft" />
+                </CardBody>
+                <FlexCardDetails className="details">
+                  <LinkExternal href={`${BASE_BSC_SCAN_URL}/address/${author}`}>Author address</LinkExternal>
+                  <Text mt="15px">Amount: {count}</Text>
+                  <Text mt="15px" color="textSubtle">Description: {description}</Text>
+                </FlexCardDetails>
+              </CardStyled>
+            )
+          })}
+        </FlexLayout>
+      }
     </>
   )
 }
